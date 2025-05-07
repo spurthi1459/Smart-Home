340d78f040ed003930009c33d7eb7b7a9b62afb4
spurthi1459 <01fe20bec108@kletech.ac.in>
2025-05-07 14:07:48 +0530

    Adding Project FIles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'chatbot.dart';
+import 'login_page.dart'; // <-- instead of chatbot.dart
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChatScreen(),
+      home: LoginPage(), // <-- Show login page first
     );
   }
 }
+
